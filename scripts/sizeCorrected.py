42d31f3ce34714b9b3e59b835642f8e8a33f9810
Sabah-hoq <skhondaker@wisc.edu>
2026-07-24 12:35:46 -0500

    iptm score visualzation will be merged to make file spras ready

--- scripts/sizeCorrected.py
+++ scripts/sizeCorrected.py
@@ -4,6 +4,7 @@ import polars as pl
 import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
+import seaborn as sns
 from load_data import load_data
 
 pairs, confidences = load_data("data/")
@@ -18,14 +19,40 @@ sizeCoreceted = (pairs.collect())["chain_pair_iptm_mean_corrected"]
 # plt.show()
 
 # filtering data
-df_filtered = pairs.filter(pl.col("ipTM_size_corrected_mean") >= 0.061) 
+df_filtered = pairs.filter(pl.col("chain_pair_iptm_mean_corrected") >= 0.061) 
 
-
+## Min and Max Values 
 max = (df_filtered.collect())["chain_pair_iptm_mean_corrected"].max()
+min = (df_filtered.collect())["chain_pair_iptm_mean_corrected"].min()
+x = (df_filtered.collect())["chain_pair_iptm_mean_corrected"]
 
+# Spras ready format 
+normalization = (x - min)/ (max - min)
 top_edges3_normal = df_filtered.with_columns(
     (pl.col("chain_pair_iptm_mean_corrected")/max)
     )
-
+# Sanity Check
 print(len(top_edges3_normal.collect()))
 print((top_edges3_normal.collect())["chain_pair_iptm_mean_corrected"].describe())
+
+# plot 
+fig, ax = plt.subplots()
+plt.hist((top_edges3_normal.collect())["chain_pair_iptm_mean_corrected"], bins=50000, color='teal', edgecolor='black', alpha=0.5)
+ax.ticklabel_format(style='plain', axis='y')
+ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+plt.show()
+
+# More figures below for visualization 
+sns.set_theme(style="whitegrid")
+fig,ax = plt.subplots(1,2, figsize=(14,5))
+sns.histplot(
+    sizeCoreceted,
+    bins = 1000,
+    kde=True,
+    color="#5157a1",
+    edgecolor = None, 
+    stat="density",)
+ax.set_title(
+    "iptm scoresize corrected (unfiltered)"
+)
+ax.legend()
